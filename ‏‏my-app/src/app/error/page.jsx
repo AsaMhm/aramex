bdd54e482c611fc4eecdd26e5551bb55087bda83
AsaMhm <asazaj54@gmail.com>
2024-11-25 00:51:36 +0300

    Update page.jsx

--- ‏‏my-app/src/app/error/page.jsx
+++ ‏‏my-app/src/app/error/page.jsx
@@ -1,1 +1,15 @@
+export default function Error() {
 
+
+  return (
+    
+    <main className={styles.main}>
+       
+  <h1>We'll be back soon!</h1>
+  <p>Our site is currently undergoing maintenance. Thank you for your patience.</p>
+
+      
+    </main>
+  )
+  
+}
